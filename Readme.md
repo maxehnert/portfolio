6eb48d0552e860ba86bb500346c7d5b4e790eba1
Max Ehnert <max.ehnert@gmail.com>
2014-12-15 20:05:05 -0500

    working on displaying my projects

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
-## Yo, I'm a Readme
+## My personal portfolio
 
-You should probably update me so people looking at your code have an idea what it is being used for :)
+This is my personal portfolio website.
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('The Iron Yard Rocks');
+$(function() {
+  $('.circle-graph').easyPieChart({
+    scaleColor: false,
+    lineWidth: 22,
+    lineCap: 'butt',
+    barColor: 'HSLA(200, 69%, 54%, 1)',
+    trackColor: 'white' ,
+    size: 175,
+    animate: 800
+  });
+});
